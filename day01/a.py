e919a3c852a38e36c584a811bbf09736aa1d45a4
BjarkiGr <bjg56@hi.is>
2022-12-01 21:40:17 +0000

    part 1

--- day01/a.py
+++ day01/a.py
@@ -1,9 +1,7 @@
-input = [line.strip() for line in open('input.txt').readlines()]
+input = [line.strip() for line in open("day01/input.txt").readlines()]
 
 num_lines = len(input)
 kcalMax = 0
-kcal2nd = 0
-kcal3rd = 0
 kcalSum = 0
 
 
@@ -12,14 +10,9 @@ for i in range(num_lines):
         kcalSum += int(input[i])
         if kcalSum > kcalMax:
             kcalMax = kcalSum
-        elif kcalSum > kcal2nd and kcalSum < kcalMax:
-            kcal2nd = kcalSum
-        elif kcalSum > kcal3rd and kcalSum < kcalMax and kcalSum < kcal2nd:
-            kcal3rd = kcalSum
     else:
         kcalSum = 0
+        
 
-print("1st: ", kcalMax)
-print("2nd: ", kcal2nd)
-print("3rd: ", kcal3rd)
-print("Top 3 sum: ", kcalMax + kcal2nd + kcal3rd)
+print("Highest calories: ", kcalMax)
+
