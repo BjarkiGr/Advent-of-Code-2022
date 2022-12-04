5c15ecd8c82f633d56b1a31080c912947c529718
BjarkiGr <bjg56@hi.is>
2022-12-04 13:16:46 +0000

    day 3 part 1 finito

--- day03/a.py
+++ day03/a.py
@@ -5,4 +5,31 @@
 # | |_) | |_| | |_| | |_) | | |_| | | | (_| |
 # | .__/ \___/ \___/| .__/|_|\___/|_|  \__,_|
 # | |               | |                      
-# |_|               |_|                      
+# |_|               |_|
+                       
+import string
+input = [line.strip() for line in open("day03/input.txt").readlines()]
+
+d = dict(zip(string.ascii_letters, range(1, 53)))
+
+totalPoints = 0
+
+
+def getPriority(x):
+    priority = d.get(x)
+    return priority
+
+def compare(a, b):
+    for i in range(len(a)):
+        for j in range(len(b)):
+            if(a[i] == b[j]):
+                return a[i]
+
+
+
+for i in range(len(input)):
+    rucksack = input[i]
+    compA, compB = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
+    totalPoints += (getPriority((compare(compA, compB))))
+        
+print(totalPoints)
