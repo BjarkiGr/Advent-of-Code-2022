d7fac26e92be248b84ae5c0708dc2988798bd51d
Bjarki <bjg56@hi.is>
2022-12-24 22:20:55 +0000

    day 4

---
+++ day04/a.py
@@ -0,0 +1,50 @@
+#         ___   ___        _  ___          _
+#        / _ \ / _ \      | |/ _ \        | |
+#  _ __ | | | | | | |_ __ | | | | |_ __ __| |
+# | '_ \| | | | | | | '_ \| | | | | '__/ _` |
+# | |_) | |_| | |_| | |_) | | |_| | | | (_| |
+# | .__/ \___/ \___/| .__/|_|\___/|_|  \__,_|
+# | |               | |
+# |_|               |_|
+
+a = []
+b = []
+
+taskAA = []
+taskAB = []
+taskBA = []
+taskBB = []
+
+counter = 0
+nacounter = 0
+
+input = open("day04/input.txt", "r")
+for group in input:
+    x, y = group.split(',', 1)
+    a.append(x)
+    b.append(y)
+
+
+for i in range(len(a)):
+    m, n = a[i].split('-', 1)
+    o, p = b[i].split('-', 1)
+    taskAA.append(m)
+    taskAB.append(n)
+    taskBA.append(o)
+    taskBB.append(p)
+
+for i in range(len(a)):
+    if taskAA[i] <= taskBA[i] and taskAB[i] >= taskBB[i]:
+        counter += 1
+        print("A includes B")
+
+    elif taskBA[i] <= taskAA[i] and taskBB[i] >= taskAB[i]:
+        counter += 1
+        print("B includes A")
+
+    else:
+        nacounter += 1
+        print("n/a")
+
+print(counter, "are fully included")
+print(nacounter, "are not included")
